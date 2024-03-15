95b96e3b9fa4e7b459d8a46255cfa1757391cae0
Adastephen <adakolestephen09@gmail.com>
2024-03-15 18:37:39 +0100

    add new

--- src/component/minor/Card.jsx
+++ src/component/minor/Card.jsx
@@ -7,13 +7,13 @@ function Card({ title, text, action, icon1, icon2 }) {
     <div className="bg-indigo-950 flex justify-between  w-full rounded-3xl py-5 shadow-md ">
       <div className="flex flex-col justify-center items-center m-3 w-full gap-3 ">
         <h3 className="text-white font-semibold h-1/3 ">{title}</h3>
-        <p className="text-indigo-400 text-xs h-1/3 flex justify-center">{text}</p>
+        <p className="text-indigo-400 text-xs h-1/3 flex justify-center px-3">{text}</p>
         <img
           src={icon1}
           alt="icon logo"
           className="w-14 h-1/3"
         />
-        <div className="text-orange-400 flex text-xs whitespace-nowrap gap-1 w-fit px-4">
+        <div className="text-orange-400 flex text-xs whitespace-nowrap gap-1 w-fit px-4 pt-0s">
           <p className="">{action}</p>
           <img
             src={icon2}
